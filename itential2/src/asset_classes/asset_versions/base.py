5f5d8333d8a8910c92bbbdc7a8ab799fc3354968
John <Tagmeh@gmail.com>
2024-12-18 18:41:48 -0600

    Removed initial implementation attempt with asset/ files

--- itential2/src/asset_classes/asset_versions/base.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Job(BaseModel):
-    pass
-
-
-class Workflow(BaseModel):
-    pass
--- itential2/src/endpoints/__init__.py
+++
@@ -1,1 +0,0 @@
-from itential2.src.endpoints.endpoint_version_factory import get_job, get_workflow, export_workflow
