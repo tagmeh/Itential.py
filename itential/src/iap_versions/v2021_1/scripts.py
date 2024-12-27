1ad76ea2c7520dfb11c370cf861b354085ebda04
tagmeh <tagmeh@gmail.com>
2024-12-27 00:18:42 -0600

    Reorganized v2021. Started scripts.py, preparing to work on v2023

---
+++ itential/src/iap_versions/v2021_1/scripts.py
@@ -0,0 +1,21 @@
+
+
+def import_repo():
+    """
+    WIP
+    Given a repository path? find all the assets and follow the Itential instance process for importing each asset type.
+    """
+    # Follow repo path
+    # Collect the assets, keeping them in their own groupings (workflows in an array of workflows)
+    # For each type of asset, call the appropriate itential instance methods
+    # Itential methods should give some sort of positive response/receipt to output to the user.
+    ...
+
+
+def scan_for_disallowed_names():
+    """
+    WIP
+    Scan the server for assets with disallowed naming schemes.
+    Things that may be disallowed: Spaces, slashes, parenthesis, symbols.
+    """
+    
